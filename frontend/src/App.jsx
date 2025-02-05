15605e04faf4e3a43cf822c4d420f634f8744990
phfuark <paulohsilva2007@gmail.com>
2025-02-05 16:26:22 -0300

    05.02.2025 - Componente Header

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+import Body from "./components/Body";
+
+
+export default function App() {
+  return (
+    < div className="flex">
+      <Header/>
+      <Body/>
+    </ div >
+  )
+}
