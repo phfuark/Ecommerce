531812247f3b8d8161ec02485c4a2a23f0598778
phfuark <paulohsilva2007@gmail.com>
2025-02-11 15:47:34 -0300

    11.02.2025 - Corrigindo antigos commits

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,5 @@
 export default function Header(){
     return(
-<<<<<<< HEAD
     <div class="w-20% h-full bg-white shadow-xl rounded-lg flex overflow-x-auto custom-scrollbar">
         <div class="h-screen px-4 border-r border-gray-300">
             <div class="h-16 flex items-center">
@@ -54,60 +53,5 @@ export default function Header(){
             </div>
         </div>
     </div>
-=======
-        <div class="w-20% h-full bg-white shadow-xl rounded-lg flex overflow-x-auto custom-scrollbar">
-            <div class="h-screen px-4 border-r border-gray-300">
-                <div class="h-16 flex items-center">
-                    <a href="#" class="w-48 mx-auto bg-blue-600 hover:bg-blue-700 flex items-center justify-center text-gray-100 py-2 rounded space-x-2 transition duration-150">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
-                        </svg>
-                        <p>New Task</p>
-                    </a>
-                </div>
-                <div class="px-2 pt-4 pb-8 ">
-                    <ul class="space-y-2">
-                        <li>
-                            <a class="hover:bg-gray-300 hover:bg-opacity-10 hover:text-blue-600 flex items-center justify-between text-gray-700 py-1.5 px-4 rounded space-x-2 cursor-pointer">
-                                <p class="flex items-center space-x-2">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
-                                    <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
-                                </svg>
-                                    <p>All Tasks</p>
-                                </p>
-                                <p class="bg-sky-500 text-gray-100 font-bold px-2 py-0.5 text-xs rounded-lg">1</p>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="hover:bg-gray-300 hover:bg-opacity-10 hover:text-blue-600 flex items-center justify-between text-gray-700 py-1.5 px-4 rounded space-x-2 cursor-pointer">
-                                <p class="flex items-center space-x-2">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar-event" viewBox="0 0 16 16">
-                                    <path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5z"/>
-                                    <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z"/>
-                                </svg>
-                                    <p>Today</p>
-                                </p>
-                                <p class="bg-sky-500 text-gray-100 font-bold px-2 py-0.5 text-xs rounded-lg">1</p>
-                            </a>
-                        </li>
-                        <li>
-                            <a class="hover:bg-gray-300 hover:bg-opacity-10 hover:text-blue-600 flex items-center justify-between text-gray-700 py-1.5 px-4 rounded space-x-2 cursor-pointer">
-                                <p class="flex items-center space-x-2">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"></path>
-                                </svg>
-                                    <p>Starred</p>
-                                </p>
-                                <p class="bg-sky-500 text-gray-100 font-bold px-2 py-0.5 text-xs rounded-lg">1</p>
-                            </a>
-                        </li>
-                        
-
-                        
-                    </ul>
-                </div>
-            </div>
-        </div>
->>>>>>> 43abfbb (06.02.2025 - Front (Estilo) finalizado)
     )
 }
